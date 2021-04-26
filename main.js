4dbf1c80020435c9dbab980775d7530939460a7e
Elena <e.cotar327@gmail.com>
2021-04-26 18:18:05 +0300

    Here is the project on dog years

---
+++ main.js
@@ -0,0 +1,25 @@
+//my actual age in human years
+const myAge = 29;
+
+//converting my first two human-years to dog-years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//the rest of my human years
+let laterYears = myAge - 2;
+
+//translating the rest of my human-years to dog-years
+laterYears *= 4;
+
+//my total age in dog-years
+myAgeInDogYears = earlyYears + laterYears;
+
+//my name written in lowercase
+let myName = 'Elena Danilova'.toLowerCase();
+
+console.log(earlyYears);
+console.log(laterYears);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
